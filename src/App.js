836027d9e8b3d6187f4b0d3fbdb7dbaa26f8f2c9
Luke Stevens <luke@velatura.cloud>
2020-05-14 12:05:44 +0100

    VelaWeb code

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Velatura Cloud Consultancy
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/luke-stevens-velatura/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
