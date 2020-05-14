905d05a32de4089d8688cc85bd7d6858047755d1
Luke Stevens <luke@velatura.cloud>
2020-05-14 12:36:08 +0100

    VelaWeb code incl build

---
+++ build/precache-manifest.cc33c8ede977305ba907310a50074ebb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5864901b290c7d9a27b516ab0bca72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8e58e3a36a1ba0454b7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c3cc3600030cce0a2a2",
+    "url": "/static/js/2.ff3194f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff3194f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e58e3a36a1ba0454b7",
+    "url": "/static/js/main.87c6e88c.chunk.js"
+  },
+  {
+    "revision": "07d42f666b70ef02f54d",
+    "url": "/static/js/runtime-main.c70f1312.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
